8d2ce09a269920268d4ac3c75d4889b72f1cc7a2
Martin Bodin <martin.bodin@ens-lyon.org>
2020-06-19 15:26:08 +0100

    Repetition and falsehood.

---
+++ theories/Repeat.v
@@ -0,0 +1,14 @@
+
+(** Continuously repeating something makes it believable. **)
+Axiom repetition : forall P : Prop, P /\ P /\ P.
+
+(** Continuously denying something makes it disappear. **)
+Theorem repeated_deny : forall P : Prop, ~ P /\ ~ P /\ ~ P.
+Proof.
+  intro. apply repetition.
+Qed.
+
+Corollary nothing_matters : False.
+Proof.
+  do 2 eapply proj2. apply repetition.
+Qed.
--- theories/Tactics.v
+++ theories/Tactics.v
@@ -1,5 +1,12 @@
 
-From AlternativeFacts Require Stupidity Logic Quantifiers Circular StrawMan Numbers.
+From AlternativeFacts Require
+     Circular
+     Logic
+     Numbers
+     Quantifiers
+     Repeat
+     StrawMan
+     Stupidity.
 Require Import Coq.Numbers.BinNums.
 
 
@@ -15,12 +22,14 @@ Ltac win :=
     | |- context [ xH ] => constr:(Numbers.French_matters)
     | |- context [ _ -> _ ] => constr:(Circular.nothing_matters)
     | |- context [ or ] => constr:(StrawMan.nothing_matters)
+    | |- context [ and ] => constr:(Repeat.nothing_matters)
     | _ : context [ not ] |- _ => constr:(Logic.nothing_matters)
     | _ : context [ ex ] |- _ => constr:(Quantifiers.nothing_matters)
     | _ : context [ S ] |- _ => constr:(Numbers.nothing_matters)
     | _ : context [ xH ] |- _ => constr:(Numbers.French_matters)
     | _ : context [ _ -> _ ] |- _ => constr:(Circular.nothing_matters)
     | _ : context [ or ] |- _ => constr:(StrawMan.nothing_matters)
+    | _ : context [ and ] |- _ => constr:(Repeat.nothing_matters)
     | _ => constr:(Stupidity.nothing_matters)
     end in
   apply t.
